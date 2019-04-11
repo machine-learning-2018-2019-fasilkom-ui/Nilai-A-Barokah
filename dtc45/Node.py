fea10db206bb733fafdad69db1ff172ea8ca15fd
adeliaherlisa <aazleeza@gmail.com>
2019-04-11 11:27:27 +0700

    Delete Node.py

--- dtc45/Node.py
+++
@@ -1,8 +0,0 @@
-class Node:
-    def __init__(self, child, parent, isLeafNode, branches, weight, errorEstimate, indexOfDataset):
-        self.child = child
-        self.parent = parent
-        self.isLeafNode = isLeafNode
-        self.branches = branches
-        self.weight = weight
-        self.errorEstimate = errorEstimate
