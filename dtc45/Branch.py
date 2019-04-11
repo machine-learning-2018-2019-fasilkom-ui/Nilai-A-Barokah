70b9e3abfab0a98831cc0408f9de601bff0f841a
adeliaherlisa <aazleeza@gmail.com>
2019-04-11 11:27:52 +0700

    Delete Branch.py

--- dtc45/Branch.py
+++
@@ -1,7 +0,0 @@
-class Branch:
-    def __init__(self, branchType, nominalValue, continueValue, label, isLeafNode):
-        self.branchType = branchType
-        self.nominalValue = nominalValue
-        self.continueValue = continueValue
-        self.label = label
-        self.isLeafNode = isLeafNode
